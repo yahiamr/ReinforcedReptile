59e8b2ef2c1703538c8bd420cb83349126ab914e
Yahia Abukhalil <yahiamrvc@gmail.com>
2024-01-29 23:22:49 +0200

    start get state implementation

--- agent.py
+++ agent.py
@@ -2,9 +2,8 @@ import torch
 import random
 import numpy as np
 from collections import deque
-from game import SnakeGameAI, Direction, Point
-from model import Linear_QNet, QTrainer
-from helper import plot
+from reptile import SnakeGameAI, Direction, Point
+
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
@@ -19,3 +18,10 @@ class Agent:
         self.memory = deque(maxlen=MAX_MEMORY) # popleft()
         self.model = Linear_QNet(11, 256, 3)
         self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
+
+    def get_state(self, game):
+        head = game.snake[0]
+        point_l = Point(head.x - 20, head.y)
+        point_r = Point(head.x + 20, head.y)
+        point_u = Point(head.x, head.y - 20)
+        point_d = Point(head.x, head.y + 20)
